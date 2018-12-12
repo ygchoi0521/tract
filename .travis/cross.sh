e6bd6f5e4428d8721b93df377f9f0837658ebf51
Mathieu Poumeyrol <kali@zoy.org>
2018-12-12 17:11:18 +0100

    only bother with dinghy for raspbian

--- .travis/cross.sh
+++ .travis/cross.sh
@@ -5,12 +5,12 @@ set -ex
 mkdir -p $HOME/cached/bin
 PATH=$HOME/cached/bin:$PATH
 
-which cargo-dinghy || cargo install --debug --root $HOME/cached cargo-dinghy
 
 sudo apt-get install git
 
 case "$PLATFORM" in
     "raspbian")
+        which cargo-dinghy || cargo install --debug --root $HOME/cached cargo-dinghy
         [ -e $HOME/cached/raspitools ] || git clone https://github.com/raspberrypi/tools $HOME/cached/raspitools
         TOOLCHAIN=$HOME/cached/raspitools/arm-bcm2708/arm-rpi-4.9.3-linux-gnueabihf
         export RUSTC_TRIPLE=arm-unknown-linux-gnueabihf
@@ -35,6 +35,4 @@ if [ -n "$AWS_ACCESS_KEY_ID" ]
 then
     TASK_NAME=`.travis/make_bundle.sh`
     aws s3 cp $TASK_NAME.tgz s3://tract-ci-builds/tasks/$PLATFORM/$TASK_NAME.tgz
-else
-    cargo dinghy --platform $PLATFORM build -p tract
 fi
