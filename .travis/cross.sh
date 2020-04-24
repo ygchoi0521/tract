c7f02f3fa94fcbd038fd556c9ef8b407268d15e8
Mathieu Poumeyrol <kali@zoy.org>
2020-04-24 13:32:01 +0200

    reduce more

--- .travis/cross.sh
+++ .travis/cross.sh
@@ -19,4 +19,4 @@ $SUDO apt-get -y update
 $SUDO apt-get -y install binutils-aarch64-linux-gnu gcc-aarch64-linux-gnu libssl-dev pkg-config
 rustup target add aarch64-unknown-linux-gnu
 export CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER=aarch64-linux-gnu-gcc
-(cd cli; cargo dinghy --platform aarch64-unknown-linux-gnu build --release --no-default-features --features onnx)
+(cd cli; cargo build --target aarch64-unknown-linux-gnu --release --no-default-features --features onnx)
