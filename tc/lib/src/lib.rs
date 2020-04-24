4e183cd41a6a2e4d784c6f4985ce27a008f4bbd2
Mathieu Poumeyrol <kali@zoy.org>
2020-04-24 15:59:34 +0200

    op.eval ?

--- tc/lib/src/lib.rs
+++ tc/lib/src/lib.rs
@@ -3,20 +3,14 @@ use tract_ndarray::*;
 
 #[derive(Debug, Clone, Default)]
 pub struct GlobalLpPool {
-    p: usize, //    data_is_nhwc: bool, // default is nchw (onnx)
-}
-
-impl GlobalLpPool {
-    pub fn new(p: usize) -> GlobalLpPool {
-        GlobalLpPool { p }
-    }
+    p: usize,
 }
 
 impl GlobalLpPool {
     fn eval_t<D: Datum + tract_core::num_traits::Float>(
         &self,
         input: Arc<Tensor>,
-    ) -> TractResult<TVec<Arc<Tensor>>> {
+        ) -> TractResult<TVec<Arc<Tensor>>> {
         let array = input.to_array_view::<D>()?;
         let n = array.shape()[0];
         let c = array.shape()[1];
@@ -27,15 +21,10 @@ impl GlobalLpPool {
         let divisor = array.len() / (n * c);
         let input = array.into_shape(((n * c), divisor))?;
         let divisor = D::from(divisor).unwrap().recip();
-        let result = if self.p == 1 {
-            input.fold_axis(Axis(1), D::zero(), |&a, &b| a + b.abs()).map(|a| *a * divisor)
-        } else if self.p == 2 {
-            input.fold_axis(Axis(1), D::zero(), |&a, &b| a + b * b).map(|a| a.sqrt() * divisor)
-        } else {
+        let result =
             input
-                .fold_axis(Axis(1), D::zero(), |&a, &b| a + b.abs().powi(self.p as i32))
-                .map(|a| a.powf(D::from(self.p).unwrap().recip()) * divisor)
-        };
+            .fold_axis(Axis(1), D::zero(), |&a, &b| a + b.abs().powi(self.p as i32))
+            .map(|a| a.powf(D::from(self.p).unwrap().recip()) * divisor);
         Ok(tvec!(result.into_shape(final_shape)?.into_arc_tensor()))
     }
 }
@@ -60,11 +49,7 @@ impl StatelessOp for GlobalLpPool {
 impl TypedOp for GlobalLpPool {
     as_op!();
 
-    fn output_facts(&self, inputs: &[&TypedFact]) -> TractResult<TVec<TypedFact>> {
-        let mut output = inputs[0].clone();
-        for i in 2..output.shape.rank() {
-            output.shape.set_dim(i, TDim::from(1))?
-        }
-        Ok(tvec!(output))
+    fn output_facts(&self, _inputs: &[&TypedFact]) -> TractResult<TVec<TypedFact>> {
+        todo!();
     }
 }
