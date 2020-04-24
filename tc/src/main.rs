bdb4798d46d6feb2f596f3dc17e68bc554174e44
Mathieu Poumeyrol <kali@zoy.org>
2020-04-24 15:27:43 +0200

    back to test against core

--- tc/src/main.rs
+++ tc/src/main.rs
@@ -10,6 +10,7 @@ fn main() -> TractResult<()> {
 */
 
 /*
+BUG !
 use tract_core::prelude::*;
 
 fn main() -> TractResult<()> {
@@ -25,9 +26,23 @@ fn main() -> TractResult<()> {
 
 use tract_core::prelude::*;
 
+fn main() -> TractResult<()> {
+    let mut model = TypedModel::default();
+    let s = model.add_source("source", TypedFact::dt_shape(DatumType::F64, [1usize, 1].as_ref())?)?;
+    model.wire_node("pool", tract_core::ops::nn::GlobalLpPool::default(), &[s])?;
+    let plan = SimplePlan::new(model)?;
+    let result = plan.run(tvec!(tensor2(&[[1.0f64, 2.0]])))?;
+    println!("{:?}", result);
+    Ok(())
+}
+
+/* NO BUG
+use tract_core::prelude::*;
+
 fn main() {
     let t = rctensor4(&[[[[12.0f64]]]]);
     let p = std::env::args().nth(1).unwrap().parse().unwrap();
     let t = tclib::eval_t::<f64>(p, t);
     println!("{:?}", t);
 }
+*/
